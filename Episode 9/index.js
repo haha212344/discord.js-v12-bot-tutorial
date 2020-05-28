17fa4f8879b1fce50df17001a8a58eb7ab01bbfe
AnonymousSportStacker <58582599+AnonymousSportStacker@users.noreply.github.com>
2020-05-27 23:30:33 -0600

    Create index.js

---
+++ Episode 9/index.js
@@ -0,0 +1,80 @@
+ const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const { token } = require('./config.json');
+
+const { readdirSync } = require('fs');
+
+const { join } = require('path');
+
+client.commands= new Discord.Collection();
+
+const prefix = ';';
+//You can change the prefix if you like. It doesn't have to be !
+
+
+const commandFiles = readdirSync(join(__dirname, "commands")).filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(join(__dirname, "commands", `${file}`));
+    client.commands.set(command.name, command);
+}
+
+
+client.on("error", console.error);
+
+client.on('ready', () => {
+    console.log('I am ready');
+    client.user.setStatus(`idle`)
+});
+
+
+let stats = {
+    serverID: '<SERVER ID>',
+    total: "<ID>",
+    member: "<ID>",
+    bots: "<ID>"
+}
+
+
+
+client.on('guildMemberAdd', member => {
+    if(member.guild.id !== stats.serverID) return;
+    client.channels.cache.get(stats.total).setName(`Total Users: ${member.guild.memberCount}`);
+    client.channels.cache.get(stats.member).setName(`Members: ${member.guild.members.cache.filter(m => !m.user.bot).size}`);
+    client.channels.cache.get(stats.bots).setName(`Bots: ${member.guild.members.cache.filter(m => m.user.bot).size}`);
+})
+
+client.on('guildMemberRemove', member => {
+    if(member.guild.id !== stats.serverID) return;
+    client.channels.cache.get(stats.total).setName(`Total Users: ${member.guild.memberCount}`);
+    client.channels.cache.get(stats.member).setName(`Members: ${member.guild.members.cache.filter(m => !m.user.bot).size}`);
+    client.channels.cache.get(stats.bots).setName(`Bots: ${member.guild.members.cache.filter(m => m.user.bot).size}`);
+
+    
+})
+
+client.on("message", async message => {
+
+    if(message.author.bot) return;
+    if(message.channel.type === 'dm') return;
+
+    if(message.content.startsWith(prefix)) {
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+
+        const command = args.shift().toLowerCase();
+
+        if(!client.commands.has(command)) return;
+
+
+        try {
+            client.commands.get(command).run(client, message, args);
+
+        } catch (error){
+            console.error(error);
+        }
+    }
+})
+
+client.login(token);
